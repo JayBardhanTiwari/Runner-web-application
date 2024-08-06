a96f6dc710db0232660e3c327260f957405a0a10
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-08-06 22:26:43 +0530

    New project

---
+++ src/main/java/com/jay/runner/runnerwebapp/RunnerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.jay.runner.runnerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jay/runner/runnerwebapp/RunnerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.runner.runnerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
